a4e36329e96ff0670d6341a67c1fc35b52b3a8b1
emtwnty <muizahsanu@gmail.com>
2020-05-02 01:27:50 +0700

    Initial commit

---
+++ app/src/main/java/com/example/sharedpreferencesabil/AppDatabase.java
@@ -0,0 +1,10 @@
+package com.example.sharedpreferencesabil;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {AktivisEntity.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    public abstract AktivisDao aktivisDao();
+}
